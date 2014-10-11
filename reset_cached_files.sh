88cc300d83103a371f84271857571682592658be
timakin <timaki.st@gmail.com>
2014-10-11 11:48:36 +0900

    reset-command add

---
+++ reset_cached_files.sh
@@ -0,0 +1,7 @@
+rm -f result_texts/*
+rm -f tmp/*
+cat /dev/null > wos_list.csv
+cat /dev/null > wos_result.csv
+cat /dev/null > if_existed_list.csv
+cat /dev/null > if_not_existed_list.csv
+cat /dev/null > unique_list.txt
